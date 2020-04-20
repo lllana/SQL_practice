7e7a976927add6e2fbd61c47ee435deb4e21862d
Lana Rubchenia <svetarubchenya@gmail.com>
2020-04-20 19:06:16 +0100

    Update Lessson3.sql

--- Lessson3.sql
+++ Lessson3.sql
@@ -91,14 +91,70 @@ SELECT 	AVG(standard_qty) mean_standard,
         AVG(poster_amt_usd) mean_poster_usd
  FROM orders;
 /*
+Though this is more advanced than what we have covered so far try finding - what
+is the MEDIAN total_usd spent on all orders? Note, this is more advanced than
+the topics we have covered thus far to build a general solution, but we can hard
+code a solution in the following way.
 */
+SELECT *
+FROM (SELECT total_amt_usd
+      FROM orders
+      ORDER BY total_amt_usd
+      LIMIT 3457) AS Table1
+ORDER BY total_amt_usd DESC
+LIMIT 2;
+
+###GroupBy
+
+/*
+Which account (by name) placed the earliest order? Your solution should have the
+account name and the date of the order.
+*/
+SELECT a.name, o.occurred_at
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+ORDER BY occurred_at
+LIMIT 1;
+
+/*
+Find the total sales in usd for each account. You should include two columns -
+the total sales for each company's orders in usd and the company name.
+*/
+
+/*
+Via what channel did the most recent (latest) web_event occur, which account was
+associated with this web_event? Your query should return only three values - the
+date, channel, and account name.
+*/
+
+/*
+Find the total number of times each type of channel from the web_events was used.
+Your final table should have two columns - the channel and the number of times
+the channel was used.
+*/
+
+/*
+Who was the primary contact associated with the earliest web_event?
+*/
+
+/*
+What was the smallest order placed by each account in terms of total usd. Provide
+only two columns - the account name and the total usd. Order from smallest dollar
+amounts to largest.
+*/
+
+/*
+Find the number of sales reps in each region. Your final table should have two
+columns - the region and the number of sales_reps. Order from fewest reps to
+most reps.
+*/
+
 /*
 */
+
 /*
 */
-/*
-*/
-/*
-*/
+
 /*
 */
