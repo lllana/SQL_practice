eeaf99a125479732e628130b14f1a49d0bcb0d1b
Lana Rubchenia <svetarubchenya@gmail.com>
2020-04-21 17:22:36 +0100

    Update Lessson3.sql

--- Lessson3.sql
+++ Lessson3.sql
@@ -294,36 +294,110 @@ FROM sales_reps;
 /*
 How many of the sales reps have more than 5 accounts that they manage?
 */
+SELECT s.name, s.id, COUNT(*) num_accounts
+FROM accounts a
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY s.name, s.id
+HAVING COUNT(*) > 5
+ORDER BY num_accounts;
 
 /*
+How many accounts have more than 20 orders?
 */
+SELECT a.name, a.id, COUNT(*) num_orders
+FROM orders o
+JOIN accounts a
+ON a.id=o.account_id
+GROUP BY a.name, a.id
+HAVING COUNT(*) > 20
+ORDER BY num_orders;
 
 /*
+Which account has the most orders?
 */
+SELECT a.name, a.id, COUNT(*) num_orders
+FROM accounts a
+JOIN orders o
+ON a.id=o.account_id
+GROUP BY a.name, a.id
+ORDER BY num_orders desc
+LIMIT 1;
 
 /*
+Which accounts spent more than 30,000 usd total across all orders?
 */
+SELECT a.name, a.id, SUM(o.total_amt_usd) total_spend
+FROM orders o
+JOIN accounts a
+ON a.id=o.account_id
+GROUP BY a.name, a.id
+HAVING SUM(o.total_amt_usd) > 30000
+ORDER BY total_spend;
 
 /*
+Which accounts spent less than 1,000 usd total across all orders?
 */
+SELECT a.name, a.id, SUM(o.total_amt_usd) total_spend
+FROM orders o
+JOIN accounts a
+ON a.id=o.account_id
+GROUP BY a.name, a.id
+HAVING SUM(o.total_amt_usd) < 1000
+ORDER BY total_spend;
 
 /*
+Which account has spent the most with us?
 */
+SELECT a.name, a.id, SUM(o.total_amt_usd) total_spend
+FROM orders o
+JOIN accounts a
+ON a.id=o.account_id
+GROUP BY a.name, a.id
+ORDER BY total_spend desc
+LIMIT 1;
 
 /*
+Which account has spent the least with us?
 */
+SELECT a.name, a.id, SUM(o.total_amt_usd) total_spend
+FROM orders o
+JOIN accounts a
+ON a.id=o.account_id
+GROUP BY a.name, a.id
+ORDER BY total_spend
+LIMIT 1;
 
 /*
+Which accounts used facebook as a channel to contact customers more than 6 times?
 */
+SELECT a.name, a.id, w.channel, COUNT(*) Num_contacts
+FROM web_events w
+JOIN accounts a
+ON a.id = w.account_id
+GROUP BY a.name, a.id, w.channel
+HAVING COUNT(*)>6 AND w.channel = 'facebook'
+ORDER BY Num_contacts;
 
 /*
+Which account used facebook most as a channel?
 */
+SELECT a.name, a.id, w.channel, COUNT(*) Num_contacts
+FROM web_events w
+JOIN accounts a
+ON a.id = w.account_id
+GROUP BY a.name, a.id, w.channel
+HAVING COUNT(*)>6 AND w.channel = 'facebook'
+ORDER BY Num_contacts desc
+LIMIT 1;
 
 /*
+Which channel was most frequently used by most accounts?
 */
-
-/*
-*/
-
-/*
-*/
+SELECT a.id, a.name, w.channel, COUNT(*) use_of_channel
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id
+GROUP BY a.id, a.name, w.channel
+ORDER BY use_of_channel DESC
+LIMIT 10;
