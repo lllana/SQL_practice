9bef96ad5df4d08f42449cca5f639aa80e177026
Lana Rubchenia <svetarubchenya@gmail.com>
2020-04-24 16:36:34 +0100

    Update Lesson4.sql

--- Lesson4.sql
+++ Lesson4.sql
@@ -77,12 +77,43 @@ GROUP BY 1;
  The total amount spent on all orders on the first month that any order was
  placed in the orders table (in terms of usd).
  */
-SELECT 	DATE_TRUNC('month', occurred_at),
-        SUM(total_amt_usd) AS total_usd
+ SELECT 	DATE_TRUNC('month', occurred_at),
+          SUM(total_amt_usd) AS total_usd
 FROM orders
 WHERE DATE_TRUNC('month', occurred_at) =
-	 (SELECT DATE_TRUNC('month', occurred_at)
-	  FROM orders
-	   ORDER BY 1
+	   (SELECT DATE_TRUNC('month', occurred_at)
+	    FROM orders
+	    ORDER BY 1
 	    LIMIT 1)
 GROUP BY 1;
+
+/*
+Provide the name of the sales_rep in each region with the largest amount of
+total_amt_usd sales.
+*/
+
+/*
+For the region with the largest (sum) of sales total_amt_usd, how many total
+(count) orders were placed?
+*/
+
+/*
+How many accounts had more total purchases than the account name which has bought
+the most standard_qty paper throughout their lifetime as a customer?
+*/
+
+/*
+For the customer that spent the most (in total over their lifetime as a customer)
+ total_amt_usd, how many web_events did they have for each channel?
+*/
+
+/*
+What is the lifetime average amount spent in terms of total_amt_usd for the top
+10 total spending accounts?
+*/
+
+/*
+What is the lifetime average amount spent in terms of total_amt_usd, including
+only the companies that spent more per order, on average, than the average of
+all orders.
+*/
