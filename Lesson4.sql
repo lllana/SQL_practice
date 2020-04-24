b97a31b23a36563e82518a9b516572520806357b
Lana Rubchenia <svetarubchenya@gmail.com>
2020-04-24 16:11:18 +0100

    Update Lesson4.sql

--- Lesson4.sql
+++ Lesson4.sql
@@ -15,13 +15,28 @@ Part 2. Match each channel to its corresponding average number of events per day
 */
 SELECT 	channel,
 		    AVG(count_events) AS Avg_count_events
-FROM
-(SELECT
-    DATE_TRUNC('day', occurred_at) AS event_date,
-		channel,
-		COUNT(*) AS count_events
-FROM web_events
-GROUP BY 1,2
- ) sub
+FROM    (SELECT DATE_TRUNC('day', occurred_at) AS event_date,
+		           channel,
+		           COUNT(*) AS count_events
+        FROM web_events
+        GROUP BY 1,2) sub
  GROUP BY 1
  ORDER BY 2 desc;
+
+ /*
+ */
+
+ /*
+ */
+
+ /*
+ */
+
+ /*
+ */
+
+ /*
+ */
+
+ /*
+ */
