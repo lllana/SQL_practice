3b2850555bb9665b0887b6f48fe210d564658395
Lana Rubchenia <svetarubchenya@gmail.com>
2020-04-27 18:25:34 +0100

    Update Lesson4.sql

--- Lesson4.sql
+++ Lesson4.sql
@@ -151,11 +151,65 @@ HAVING SUM(o.total_amt_usd) = (
 How many accounts had more total purchases than the account name which has bought
 the most standard_qty paper throughout their lifetime as a customer?
 */
+SELECT COUNT(*)
+FROM (
+  SELECT a.name account_name
+  FROM accounts a
+  JOIN orders o
+  ON a.id=o.account_id
+  GROUP BY 1
+  HAVING SUM(o.total) >
+    (SELECT total
+    FROM
+      (SELECT a.name account_name, SUM(o.standard_qty) standard_qt, SUM(o.total) total
+      FROM accounts a
+      JOIN orders o
+      ON a.id=o.account_id
+      GROUP BY 1
+      ORDER BY 2 desc
+      LIMIT 1) inner_Sub)
+    )counter_tab
+
 
 /*
 For the customer that spent the most (in total over their lifetime as a customer)
  total_amt_usd, how many web_events did they have for each channel?
 */
+SELECT a.name account_name, w.channel, COUNT(w.*)
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id
+GROUP BY 1,2
+HAVING a.name =
+  (SELECT account_name
+  FROM
+      (SELECT a.name account_name, SUM(o.total_amt_usd) total_sales
+      FROM accounts a
+      JOIN orders o
+      ON a.id=o.account_id
+      GROUP BY 1
+      ORDER BY 2 DESC
+      LIMIT 1)sub
+  )
+ORDER BY 3 desc;
+
+/* OR
+*/
+
+SELECT a.name, w.channel, COUNT(*)
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id AND a.id =  (SELECT id
+                     FROM (SELECT a.id, a.name, SUM(o.total_amt_usd) tot_spent
+                           FROM orders o
+                           JOIN accounts a
+                           ON a.id = o.account_id
+                           GROUP BY a.id, a.name
+                           ORDER BY 3 DESC
+                           LIMIT 1) inner_table)
+GROUP BY 1, 2
+ORDER BY 3 DESC;
+
 
 /*
 What is the lifetime average amount spent in terms of total_amt_usd for the top
