85f68da506fdb27264d922755115bdfba0a9ac1f
Lana Rubchenia <svetarubchenya@gmail.com>
2020-04-28 13:31:13 +0100

    Update Lesson5.sql

--- Lesson5.sql
+++ Lesson5.sql
@@ -57,18 +57,59 @@ FROM (
 
 ###POSITION, STRPOS, & SUBSTR
 /*
+Use the accounts table to create first and last name columns that hold the first
+and last names for the primary_poc.
+*/
+SELECT
+    LEFT(primary_poc,STRPOS(primary_poc,' ')) AS name_pr_poc,
+    RIGHT(primary_poc, LENGTH(primary_poc)- STRPOS(primary_poc,' ')) AS surname_pr_poc
+FROM accounts;
+
+/*
+Now see if you can do the same thing for every rep name in the sales_reps table.
+Again provide first and last name columns.
+*/
+SELECT
+    LEFT(name, STRPOS(name,' ')) AS name,
+    RIGHT (name, LENGTH(name)-STRPOS(name,' ')) AS surname
+FROM sales_reps;
+
+###CONCAT
+/*
+ach company in the accounts table wants to create an email address for each
+primary_poc. The email address should be
+the first name of the primary_poc . last name primary_poc @ company name .com.
+*/
+SELECT
+CONCAT (LEFT(primary_poc, STRPOS(primary_poc, ' ')-1),'.',
+RIGHT(primary_poc, LENGTH(primary_poc)-STRPOS(primary_poc,' ' )), '@',name,'.com')
+FROM accounts;
+
+WITH t1 AS (
+   SELECT LEFT(primary_poc,STRPOS(primary_poc, ' ') -1 ) first_name,
+          RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name,
+          name
+   FROM accounts)
+SELECT first_name, last_name, CONCAT(first_name, '.', last_name, '@', name, '.com')
+FROM t1;
+
+
+/*
+You may have noticed that in the previous solution some of the company names
+include spaces, which will certainly not work in an email address. See if you
+can create an email address that will work by removing all of the spaces in the
+account name, but otherwise your solution should be just as in question 1. Some
+helpful documentation is here.
 */
 
 /*
-*/
-
-/*
-*/
-
-/*
-*/
-
-/*
+We would also like to create an initial password, which they will change after
+their first log in. The first password will be the first letter of
+the primary_poc's first name (lowercase), then the last letter of their first
+name (lowercase), the first letter of their last name (lowercase), the last
+letter of their last name (lowercase), the number of letters in their first name,
+the number of letters in their last name, and then the name of the company they
+are working with, all capitalized with no spaces.
 */
 
 /*
