dafaea31a4e5a29e002df9446b9f948e36c9dc61
Lana Rubchenia <svetarubchenya@gmail.com>
2020-04-28 14:57:07 +0100

    Update Lesson5.sql

--- Lesson5.sql
+++ Lesson5.sql
@@ -139,6 +139,12 @@ CONCAT(LEFT(t1.pr_poc_name,1),
 FROM t1;
 
 ###CAST
+
+SELECT
+  DATE as origine_date,
+  (SUBSTR(date,7,4)||'-'||LEFT(date,2)||'-'||SUBSTR(date,4,2))::DATE new_date
+FROM sf_crime_data
+
 /*
 */
 
