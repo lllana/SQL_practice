d0fef558fd8ea13fb1b003650d70d2e612653930
Lana Rubchenia <svetarubchenya@gmail.com>
2020-04-30 11:32:32 +0100

    Update Lesson6.sql

--- Lesson6.sql
+++ Lesson6.sql
@@ -114,7 +114,38 @@ WINDOW account_year_window AS (PARTITION BY account_id ORDER BY DATE_TRUNC('year
 ###Comparing a Row to Previous Row
 
 /*
+Modify Derek's query from the previous video in the SQL Explorer below to perform
+this analysis. You'll need to use occurred_at and total_amt_usd in the orders
+table along with LEAD to do so. In your query results, there should be four
+columns: occurred_at, total_amt_usd, lead, and lead_difference.
 */
+
+SELECT account_id,
+       standard_sum,
+       LAG(standard_sum) OVER (ORDER BY standard_sum) AS lag,
+       LEAD(standard_sum) OVER (ORDER BY standard_sum) AS lead,
+       standard_sum - LAG(standard_sum) OVER (ORDER BY standard_sum) AS lag_difference,
+       LEAD(standard_sum) OVER (ORDER BY standard_sum) - standard_sum AS lead_difference
+FROM (
+SELECT account_id,
+       SUM(standard_qty) AS standard_sum
+  FROM orders
+ GROUP BY 1
+ ) sub
+
+
+ 
+SELECT occurred_at,
+       total_amt_usd ,
+       LEAD(total_amt_usd) OVER (ORDER BY occurred_at ) AS lead,
+       LEAD(total_amt_usd ) OVER (ORDER BY occurred_at ) - total_amt_usd  AS lead_difference
+FROM (
+SELECT occurred_at,
+       SUM(total_amt_usd ) AS total_amt_usd
+  FROM orders
+ GROUP BY 1
+ ) sub
+
 
 /*
 */
