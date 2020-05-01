1e8f8418f4b2b4ce3a41886182e362a13fecd430
Lana Rubchenia <svetarubchenya@gmail.com>
2020-05-01 15:25:48 +0100

    Update Lesson7.sql

--- Lesson7.sql
+++ Lesson7.sql
@@ -101,14 +101,66 @@ ORDER BY w1.account_id,  w1.occurred_at
 
 ###UNIONs
 /*
+Write a query that uses UNION ALL on two instances (and selecting all columns)
+of the accounts table. Then inspect the results and answer the subsequent quiz.
 */
 
+SELECT *
+    FROM accounts
+
+UNION ALL
+
+SELECT *
+  FROM accounts
 
 /*
+Add a WHERE clause to each of the tables that you unioned in the query above,
+filtering the first table where name equals Walmart and filtering the second
+table where name equals Disney. Inspect the results then answer the subsequent
+quiz.
 */
 
+SELECT *
+FROM accounts
+WHERE name = 'Walmart'
+
+UNION
+
+SELECT *
+FROM accounts
+WHERE name = 'Disney'
+
 /*
+OR
 */
+
+SELECT *
+FROM accounts
+WHERE name = 'Walmart' OR name = 'Disney'
+
+UNION
+
+SELECT *
+FROM accounts
+WHERE name = 'Walmart' OR name = 'Disney'
+
+/*
+Perform the union in your first query (under the Appending Data via UNION header)
+in a common table expression and name it double_accounts. Then do a COUNT the
+number of times a name appears in the double_accounts table. If you do this
+correctly, your query results should have a count of 2 for each name.
+*/
+WITH double_accounts AS (
+SELECT *
+    FROM accounts
+UNION ALL
+SELECT *
+  FROM accounts)
+
+SELECT Name,
+        COUNT(name) AS count_names
+FROM double_accounts
+GROUP BY 1
 
 /*
 */
