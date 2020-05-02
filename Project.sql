34e2ac2585d4e16e145b1856767be32651690239
Lana Rubchenia <svetarubchenya@gmail.com>
2020-05-02 18:32:37 +0100

    Update Project.sql

--- Project.sql
+++ Project.sql
@@ -72,6 +72,10 @@ JOIN actor a
 ON fa.actor_id = a.actor_id
 ORDER BY 3;
 
+/*
+OR
+*/
+
 SELECT full_name,
        filmtitle,
        filmlen,
@@ -90,7 +94,7 @@ FROM
         ON fa.actor_id = a.actor_id
         JOIN film f
         ON f.film_id = fa.film_id) t1
-        
+
 /*
 Now, we bring in the advanced SQL query concepts!
 Revise the query you wrote above to create a count of movies in each of
@@ -99,21 +103,17 @@ the 4 filmlen_groups:
 
 Match the count of movies in each filmlen_group.
 */
-SELECT
-    (a.first_name)||' '||(a.last_name) AS full_name,
-    f.title,
-    f.length,
+
+SELECT DISTINCT(filmlen_groups),
+      COUNT(title) OVER (PARTITION BY filmlen_groups) AS filmcount_bylencat
+FROM
+    (SELECT title, length,
     CASE WHEN f.length > 180 THEN 'More than 3 hours'
         WHEN f.length > 120 THEN 'Between 2-3 hours'
         WHEN f.length > 60 THEN 'Between 1-2 hours'
         ELSE '1 hour or less' END AS filmlen_groups
-
-FROM film f
-JOIN film_actor fa
-ON fa.film_id = f.film_id
-JOIN actor a
-ON fa.actor_id = a.actor_id
-ORDER BY 3;
+FROM film f) t1
+ORDER BY 1
 
 /*
 */
